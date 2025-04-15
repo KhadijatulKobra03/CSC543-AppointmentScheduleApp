31b98903e67d236b4801b0e98279e48b2826834a
Kate <115410415+LoveyThePigeon@users.noreply.github.com>
2025-04-14 23:27:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const http = require('http');
+const url = require('url');
+readFileModule = require('./public_html/js/readFileModule.js'); 
+
+serving = function (req, res) {
+	console.log(req.url);
+	let fileName = './public_html' + url.parse(req.url).pathname;
+	console.log(fileName);
+	// call readFileModule.js
+	readFileModule.readFile(fileName,res);
+}
+
+// creating server object
+const myServer = http.createServer(serving);
+
+// server starts listenig on port 80
+myServer.listen(80, function() {console.log("Listening on port 80")});
 
