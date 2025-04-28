5f608d04b603150ba4c2725152b75f945806afc0
marinadiaz2020 <marinalara.daltro@gmail.com>
2025-04-28 19:21:10 -0400

    Update schedule.js

--- public_html/JS/schedule.js
+++ public_html/JS/schedule.js
@@ -1,1 +1,13 @@
+function availableSlot () {
+    const freeSlot = document.querySelectorAll(".day");
+
+    freeSlot.forEach(slot => {
+    slot.addEventListener("click", function(){
+    const bookingForm = new bootstrap.Modal(document.getElementById("bookingClass"));
+    bookingForm.show();
+    });
+   
+    });
+}
+availableSlot();
 
