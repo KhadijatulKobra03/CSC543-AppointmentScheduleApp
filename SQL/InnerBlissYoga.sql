a4c7d1083dc8095af8cf28c444a64a13a661aac6
Khadijatul Kobra <132243344+KhadijatulKobra03@users.noreply.github.com>
2025-04-29 19:03:41 -0400

    Update InnerBlissYoga.sql

--- SQL/InnerBlissYoga.sql
+++ SQL/InnerBlissYoga.sql
@@ -18,3 +18,23 @@ INSERT INTO newsletter_signup (newsletter_user_id, newsletter_first_name, newsle
 (1, 'John', 'Smith', 'jsmith@email.com'),
 (2, 'Bob', 'Brown', 'bobby@gmail.com');
 
+
+--Khadija's 
+
+Create Database yoga_database;
+use yoyga_user;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50),
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(20)
+);
+
+CREATE TABLE event_registrations (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  event_name VARCHAR(100) NOT NULL,
+  registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
